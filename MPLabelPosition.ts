6793fef018daadf16c4638d2ae371ca946f9af30
Tim Mikkelsen <gitlab@mapspeople>
2024-12-09 11:49:40 +0000

    Pushing version release/2.2.0

---
+++ MPLabelPosition.ts
@@ -0,0 +1,12 @@
+/**
+ * The possible position of the label, anchored on the icon.
+ *
+ * @export
+ * @enum {number}
+ */
+export enum MPLabelPosition {
+    left,
+    top,
+    bottom,
+    right
+}
